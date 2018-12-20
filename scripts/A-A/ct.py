4e26086b2c0338db980a0a3e736c499acf51ef15
Bram Staps-Wintermans <bstaps@bol.com>
2018-12-20 12:55:28 +0100

    x

--- scripts/A-A/ct.py
+++ scripts/A-A/ct.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 import requests
+import re
 import sys
+
+TABLE_PARSE_REGEX = "\s+<TR>\s*(<TD[^>]*>[\s\S]*?</TD>)*\s*</TR>"
+
+def get_and_parse_ct(domain, wildcard = False):
+    cts = { 
+        False: "https://crt.sh?q={}",
+        True: "https://crt.sh?q=%25.{}"
+    }
+
+    ct_url = cts[wildcard].format(domain)
+
+    def parse_regex_output(found_elem):
+        return filter(lambda x: x, map(lambda s: s.strip()[1:-1].split(">")[-1].split("<")[0], found_elem.group(1).split("\n"))) + ([domain] if not wildcard else [])
+
+    ret = requests.get(ct_url)
+    return map(parse_regex_output, re.finditer(TABLE_PARSE_REGEX, ret.text))
+
+def getct(domain):
+    return get_and_parse_ct(domain) + get_and_parse_ct(domain, True)
 
 domains = set()
 for line in sys.stdin:
     domain = line.strip()
-    certs = requests.get("https://certspotter.com/api/v0/certs?domain=%s" % domain).json()
-
-    try:
-        for cert in certs:
-            for domain in cert["dns_names"]:
-                domain = domain.replace("*.", "")
-                domains.add(domain)
-    except:
-        pass
-
+    for _,_,_,d in getct(domain):
+        domains.add(d.replace("*.", ""))
+        
 for d in domains:
     print d
